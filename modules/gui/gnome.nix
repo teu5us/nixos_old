10b1ab9c180daa2f293a534de9c315d5e6650527
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-02-14 21:17:18 +0600

    add gnome extensions

--- modules/gui/gnome.nix
+++ modules/gui/gnome.nix
@@ -10,4 +10,18 @@
       gnome.enable = true;
     };
   };
+
+  environment.systemPackages = with pkgs.gnomeExtensions; [
+    gnome-40-ui-improvements
+    transparent-window-moving
+    quake-mode
+    clipboard-indicator
+    blur-my-shell
+    sound-output-device-chooser
+    bluetooth-quick-connect
+    tray-icons-reloaded
+    unite
+    floating-dock
+    gsconnect
+  ];
 }
