b33a98e3f16c55215b5f93c7da4fe056084d3f1b
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-02-19 14:30:21 +0600

    add session commands for WMs

--- modules/gui/gnome.nix
+++ modules/gui/gnome.nix
@@ -7,6 +7,14 @@
       gdm.enable = true;
       gdm.wayland = false;
       defaultSession = "gnome-xorg";
+      sessionCommands = ''
+        # keep alacritty font size normal
+        export WINIT_X11_SCALE_FACTOR=1
+
+        # set backround image and cursor
+        ${pkgs.feh}/bin/feh --bg-scale ~/.config/wall.jpg &
+        xsetroot -cursor_name left_ptr &
+      '';
     };
     desktopManager = {
       gnome.enable = true;
