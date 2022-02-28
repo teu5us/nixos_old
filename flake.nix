17cdb4c9e95d41378e7d39a58e423145006ffc0f
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-02-28 17:52:06 +0600

    add readme

--- flake.nix
+++ flake.nix
@@ -1,20 +1,3 @@
-# https://www.tweag.io/blog/2020-07-31-nixos-flakes/
-
-# To switch from channels to flakes execute:
-# cd /etc/nixos
-# sudo wget -O flake.nix https://gist.githubusercontent.com/misuzu/80af74212ba76d03f6a7a6f2e8ae1620/raw/flake.nix
-# sudo sed -i "s/myhost/$(hostname)/g" flake.nix
-# sudo git init
-# sudo git add . # won't work without this
-# nix run nixpkgs.nixFlakes -c sudo nix --experimental-features 'flakes nix-command' build .#nixosConfigurations.$(hostname).config.system.build.toplevel
-# sudo ./result/bin/switch-to-configuration switch
-
-# Now nixos-rebuild can use flakes:
-# sudo nixos-rebuild switch --flake /etc/nixos
-
-# To update flake.lock run:
-# sudo nix flake update --commit-lock-file /etc/nixos
-
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
 
