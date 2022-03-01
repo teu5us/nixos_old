8308bfaa683a76cf96308647e090d89f6c94d940
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-03-01 12:54:37 +0600

    rewrite inputs as attrset

--- flake.nix
+++ flake.nix
@@ -1,16 +1,24 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/master";
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-  inputs.kmonad.url = "github:kmonad/kmonad?dir=nix";
-  inputs.kmonad.inputs.nixpkgs.follows = "nixpkgs";
+    kmonad = {
+      url = "github:kmonad/kmonad?dir=nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-  inputs.nix-store-emacs-packages.url = "github:teu5us/nix-store-emacs-packages";
-  inputs.nix-store-emacs-packages.inputs.nixpkgs.follows = "nixpkgs";
+    nix-store-emacs-packages = {
+      url = "github:teu5us/nix-store-emacs-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-  inputs.emacs-overlay.url = "github:nix-community/emacs-overlay?rev=396182cd543073db96ea696bd2a41cb24e87781f";
+    emacs-overlay.url = "github:nix-community/emacs-overlay?rev=396182cd543073db96ea696bd2a41cb24e87781f";
+  };
 
   outputs = inputs:
     let baseSystem = import ./base-system.nix inputs;
