8cbf5e2c23a4e289e294d7e3cd8705aa10f40c80
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-09-01 11:26:17 +0600

    update hash

--- packages/spoof-dpi/default.nix
+++ packages/spoof-dpi/default.nix
@@ -7,5 +7,5 @@ buildGoModule rec {
     url = "https://github.com/xvzc/${pname}/archive/refs/tags/${version}.tar.gz";
     sha256 = "0kpm7ggva3c9pfbp2yzkwcmhjfx06siqfs1qy771k9b3h6674hfn";
   };
-  vendorSha256 = "sha256-2R0lZJyoUo+K+FjN4lfvBEiNMWmsokbKGGRdYk7MZNU=";
+  vendorSha256 = "sha256-ib9xRklkLfrDCuLf7zDkJE8lJiNiUMPZ01MDxvqho6o=";
 }
