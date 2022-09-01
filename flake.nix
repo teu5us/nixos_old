ced8d880d874b611dc8b2c0843b86d1259982ba2
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-09-01 14:21:17 +0600

    home-manager 22.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
