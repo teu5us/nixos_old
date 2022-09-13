9cc4a5cca8d837fd2922e3a00f7f4a86bd219c75
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-09-13 15:17:19 +0600

    use my lens-vim fork

--- packages/default.nix
+++ packages/default.nix
@@ -9,4 +9,17 @@ self: super: {
     fetchPypi = super.python3.pkgs.fetchPypi;
   };
   easystroke = super.callPackage ./easystroke {};
+  vimPlugins = super.vimPlugins.extend (self': super': {
+    lens-vim = self.vimUtils.buildVimPluginFrom2Nix {
+      pname = "lens.vim";
+      version = "2022-09-13";
+      src = self.fetchFromGitHub {
+        owner = "Teu5us";
+        repo = "lens.vim";
+        rev = "43ad41c9fbe3add97aaff0294af74b59191de85a";
+        sha256 = "0sbj24pg1286sk3wkqbj66vddiysiq5lp46ykqnv6alkr90ng8p5";
+      };
+      meta.homepage = "https://github.com/Teu5us/lens.vim/";
+    };
+  });
 }
